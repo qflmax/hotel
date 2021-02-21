f4da18da75d059ab2556029521ed30a393c57fa8
qflmax <79366304+qflmax@users.noreply.github.com>
2021-02-21 21:52:40 +0700

    проверка работы отправки почты. вывод в консоль

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,19 @@ $(function(){
 	$('.sing-in').click(function(){
 		$('.sign-prof').css('display','block');
 		$('.sign-btns').css('display','none');
-	})
+	});
 	$('.sing-up').click(function(){
 		alert('Рега')
-	})
+	});
+	$('input[type="image"]').click(function(){
+		var email = $('input[type="email"]').val();
+
+		if (email == "") {
+			alert('Введите Email')
+		}
+		else {
+			console.log(email)
+			alert('Email отправлен')
+		}
+	});
 });
